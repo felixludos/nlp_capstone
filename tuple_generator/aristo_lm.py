3001790bc79bff7960aba568eae0eca408471117
Sam Gehman <sgehman@cs.uw.edu>
2019-05-07 11:22:11 -0700

    Add TODOs for aristo lm

---
+++ tuple_generator/aristo_lm.py
@@ -0,0 +1,7 @@
+"""
+TODO: Language model
+1. Train language model on Aristo tuples
+2. Generate tuples with OpenIE
+3. Take all predicates from OpenIE, and map them to closest Aristo predicate using pre-trained BERT embedding space
+4. Use language model on pre-processed OpenIE tuples
+"""
