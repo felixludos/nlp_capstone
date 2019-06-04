13fd78197143a1477e1111d492f05a950d45ed5f
Sam Gehman <sgehman@cs.uw.edu>
2019-06-04 16:39:43 -0700

    Add aristo corpus and fix openie

--- tuple_generator/openbook_tuple_generator.py
+++ tuple_generator/openbook_tuple_generator.py
@@ -1,10 +1,12 @@
-import open_ie_api
+from tuple_generator import open_ie_api
 
-openbook_path = 'data/openbook_corpus.txt'
-with open(openbook_path, 'r') as f:
+input_path = 'data/openbook/openbook_corpus.txt'
+output_path = 'data/openbook_tuples.tsv'
+
+with open(input_path, 'r') as f:
     facts = [line.strip()[1:-1] for line in f.readlines()]
 
 tuples = open_ie_api.call_api_many(facts, verbose=True)
 tuples = ("\t".join(part.strip() for part in t) for t in tuples)
-with open("data/openbook_tuples.tsv", 'w') as f:
+with open(output_path, 'w') as f:
     print(*tuples, sep='\n', file=f)
