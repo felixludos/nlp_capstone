f93914fba82de0a10aa36f16caca4814517dc1d1
Sam Gehman <sgehman@cs.uw.edu>
2019-06-04 18:35:38 -0700

    add argparse

--- tuple_generator/openbook_tuple_generator.py
+++ tuple_generator/openbook_tuple_generator.py
@@ -1,12 +1,16 @@
+import argparse
+
 from tuple_generator import open_ie_api
 
-input_path = 'data/openbook/openbook_corpus.txt'
-output_path = 'data/openbook_tuples.tsv'
+parser = argparse.ArgumentParser(description='Generate tuples using OpenIE.')
+parser.add_argument('input_path', type=str)
+parser.add_argument('output_path', type=str)
+args = parser.parse_args()
 
-with open(input_path, 'r') as f:
+with open(args.input_path, 'r') as f:
     facts = [line.strip()[1:-1] for line in f.readlines()]
 
 tuples = open_ie_api.call_api_many(facts, verbose=True)
 tuples = ("\t".join(part.strip() for part in t) for t in tuples)
-with open(output_path, 'w') as f:
+with open(args.output_path, 'w') as f:
     print(*tuples, sep='\n', file=f)
